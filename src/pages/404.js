a8eb4da047cfcec3ba9c34b16c4b67bd47ce8e4c
Ubuntu <ubuntu@ip-172-26-5-126.ec2.internal>
2020-07-06 20:27:43 +0000

    Update SEO mentions

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,9 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
-
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404" />
+  <Layout seo="404">
     <h1>404</h1>
     <h2>There's nothing here.</h2>
   </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
+import { Link } from "gatsby"
+
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
