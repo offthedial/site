af174551cdbaef6f4b0e9dd10e85380f7f71347e
Ubuntu <ubuntu@ip-172-26-5-126.ec2.internal>
2020-07-06 21:59:21 +0000

    add styles for links and blockquotes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,7 @@
 import Typography from "typography"
+import gray from "gray-percentage"
+
+import colors from "../static/colors"
 
 const typography = new Typography({
     googleFonts: [
@@ -12,6 +15,24 @@ const typography = new Typography({
     headerWeight: "500",
     bodyWeight: 400,
     boldWeight: 500,
+    overrideStyles: ({ rhythm }) => {
+        const styles = {
+            a: {
+                color: colors.dark_blue,
+                textDecoration: "none",
+            },
+            "a:hover": {
+                opacity: "75%",
+            },
+            blockquote: {                
+                color: gray(41),
+                marginLeft: rhythm(1 / 6),
+                paddingLeft: rhythm(6 / 9),
+                borderLeft: `${rhythm(1 / 9)} solid ${colors.dark_blue}`,
+            },
+        }
+        return styles
+    }
 })
 
 export default typography
