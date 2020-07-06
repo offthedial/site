dfacfb3354c3acf4d82e9f28f37c8d3c8d239559
Ubuntu <ubuntu@ip-172-26-5-126.ec2.internal>
2020-07-06 17:56:21 +0000

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <h1>404</h1>
+    <h2>There's nothing here.</h2>
   </Layout>
 )
 
