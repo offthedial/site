3881b05820cce68a81a29642c3e48f39d8c31015
Ubuntu <ubuntu@ip-172-26-5-126.ec2.internal>
2020-07-07 07:02:20 +0000

    Add discord redirect

---
+++ src/components/redirect.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Redirect = ({ to }) => {
+  const content = `0; url=${to}`
+  return (
+    <meta http-equiv="refresh" content={content}/>
+  )
+}
+
+export default Redirect
---
+++ src/pages/discord.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Redirect from "src/components/redirect"
+
+const Discord = () => (
+  <Redirect to="https://discord.gg/xWkx8SZ"/>
+)
+
+export default Discord
