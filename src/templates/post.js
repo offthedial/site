e511f49cbafe7364b959a813f9672f64d0d72a8d
Ubuntu <ubuntu@ip-172-26-5-126.ec2.internal>
2020-07-07 03:26:51 +0000

    Create page for each post

---
+++ src/templates/post.js
@@ -0,0 +1,55 @@
+import React from "react"
+import gray from "gray-percentage"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+
+import { rhythm } from "src/utils/typography"
+import Layout from "src/components/layout"
+
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1
+            css={css`
+              margin-bottom: ${rhythm(1 / 4)};
+            `}
+          >
+            {post.frontmatter.title}{" "}
+            <span
+              css={css`
+                color: ${gray(35)};
+                font-size: ${rhythm(1)};
+              `}
+            >
+              — {post.frontmatter.date}
+            </span>
+        </h1>
+        <h2
+          css={css`
+            color: ${gray(35)};
+            font-size: ${rhythm(.8)}
+          `}
+        >
+          Written By: {post.frontmatter.author}
+        </h2>
+        {/* Actual Post */}
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        author
+      }
+    }
+  }
+`
