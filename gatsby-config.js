e76b6f3b15365159eb1f09c68c3abd17df4a4de6
Ubuntu <ubuntu@ip-172-26-5-126.ec2.internal>
2020-07-07 01:24:04 +0000

    Update plugin order

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,22 @@ module.exports = {
     description: `Off the Dial is a unique tournament organisation for Splatoon 2, dedicated to providing fresh tournament opportunities for free agents and teams alike.`,
   },
   plugins: [
+    'gatsby-plugin-root-import',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/pages/posts`,
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    'gatsby-plugin-root-import'
+
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
   ],
 }
