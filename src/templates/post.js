bba819fdbcc83085aa7c7ecd60b829cbde8c53b2
LeptoFlare <leptoflare@gmail.com>
2020-07-07 20:30:39 -0400

    Add page title to post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { css } from "@emotion/core"
 
 import { rhythm, gray } from "src/utils/typography"
 import Layout from "src/components/layout"
@@ -8,32 +7,17 @@ import Layout from "src/components/layout"
 export default function Post({ data }) {
   const post = data.markdownRemark
   return (
-    <Layout>
+    <Layout pageTitle={" Posts | " + post.frontmatter.title} >
       <div>
-        <h1
-          css={css`
-            margin-bottom: ${rhythm(1 / 4)};
-          `}
-        >
+        <h1 style={{ marginBottom: rhythm(1 / 4) }}>
           {post.frontmatter.title}{" "}
-          <span
-            css={css`
-              color: ${gray(33)};
-              font-size: ${rhythm(1)};
-            `}
-          >
+          <span style={{ color: gray(33), fontSize: rhythm(1) }}>
             — {post.frontmatter.date}
           </span>
         </h1>
-        <h2
-          css={css`
-            color: ${gray(33)};
-            font-size: ${rhythm(0.8)};
-          `}
-        >
+        <h2 style={{ color: gray(33), fontSize: rhythm(0.8) }}>
           Written By: {post.frontmatter.author}
         </h2>
-        {/* Actual Post */}
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
