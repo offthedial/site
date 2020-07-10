576dc8407b0fbcc8f3f427cb2bccf5e2c1cbe9e8
LeptoFlare <leptoflare@gmail.com>
2020-07-09 22:53:31 -0400

    Add redirects

---
+++ src/pages/twitch.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Redirect from "src/components/redirect"
+
+const Discord = () => <Redirect to="https://twitch.tv/offthedial" />
+
+export default Discord
---
+++ src/pages/twitter.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Redirect from "src/components/redirect"
+
+const Discord = () => <Redirect to="https://twitter.com/off_the_dial" />
+
+export default Discord
