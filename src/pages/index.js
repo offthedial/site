6b9a73498b5e0ca4fbc58c293773fb35b7d940d7
LeptoFlare <leptoflare@gmail.com>
2020-07-09 23:12:52 -0400

    Refactor index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import emoji from "react-easy-emoji"
 import { Link } from "gatsby"
 import { CardColumns, Card, Row, Col } from "react-bootstrap"
 
-import Layout from "src/components/layout"
-import Footer from "src/components/footer"
+import Layout from "src/components/Layout"
+import Footer from "src/components/Footer"
 
 const Index = () => (
   <Layout pageTitle="Welcome">
@@ -34,91 +34,32 @@ const Index = () => (
         <br />
         <h2>Important Pages</h2>
         <CardColumns className="mb-3" style={{ columnCount: 1 }}>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <h3>
-                  <Link to="faq">FAQ</Link>
-                </h3>
-              </Card.Title>
-              <Card.Text>
-                We get asked a lot of questions, so a FAQ necessary. We highly
-                recommend you check out this page.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <h3>
-                  <Link to="posts">Posts</Link>
-                </h3>
-              </Card.Title>
-              <Card.Text>
-                Stay up to date with the latest blog posts, news, and updates on
-                everything Off the Dial!
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <CardPage title="FAQ" link="faq">
+            We get asked a lot of questions, so a FAQ necessary. We highly recommend you check out this page.
+          </CardPage>
+          <CardPage title="Posts" link="posts">
+            Stay up to date with the latest blog posts, news, and updates on everything Off the Dial!
+          </CardPage>
         </CardColumns>
       </Col>
     </Row>
     <h2 id="media">Media</h2>
     <h2>More Information</h2>
     <Row>
-      <Col xl="4" md="6" className="mb-3">
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <h3>
-                <Link to="bot">Off the Dial Bot</Link>
-              </h3>
-            </Card.Title>
-            <blockquote style={{ fontSize: "88%" }}>
-              <Card.Text>
-                Our server has a custom Off the Dial Bot used for tournament
-                running assistance. If you would like to know more, we have a
-                page dedicated to about the bot right here!
-              </Card.Text>
-            </blockquote>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col xl="4" md="6" className="mb-3">
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <h3>
-                <Link to="community">Community</Link>
-              </h3>
-            </Card.Title>
-            <blockquote style={{ fontSize: "88%" }}>
-              <Card.Text>
-                Off the Dial is thankful to have such an awesome team and
-                community! And you can check each one of them out right here!
-              </Card.Text>
-            </blockquote>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col xl="4" md="6" className="mb-3">
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <h3>
-                <a href="https://github.com/offthedial/assets">Assets</a>
-              </h3>
-            </Card.Title>
-            <blockquote style={{ fontSize: "88%" }}>
-              <Card.Text>
-                Many of our assets are public and free. Assets include logos and
-                banners related to Off the Dial and its tournaments. You can
-                find them all right here!
-              </Card.Text>
-            </blockquote>
-          </Card.Body>
-        </Card>
-      </Col>
+      <CardPageOther title="Off the Dial Bot" link="bot">
+        Our server has a custom Off the Dial Bot used for tournament
+        running assistance. If you would like to know more, we have a
+        page dedicated to about the bot right here!
+      </CardPageOther>
+      <CardPageOther title="Community" link="community">
+        Off the Dial is thankful to have such an awesome team and
+        community! And you can check each one of them out right here!
+      </CardPageOther>
+      <CardPageOther title="Assets" link="assets">
+        Many of our assets are public and free. Assets include logos and
+        banners related to Off the Dial and its tournaments. You can
+        find them all right here!
+      </CardPageOther>
     </Row>
     <Footer>
       Built with{" "}
@@ -127,6 +68,40 @@ const Index = () => (
       in beta, more features are to come. Stay tuned!
     </Footer>
   </Layout>
+)
+
+const CardPage = ({ title, link, children }) => (
+  <Card>
+    <Card.Body>
+      <Card.Title>
+        <h3>
+          <Link to={ link }>{ title }</Link>
+        </h3>
+      </Card.Title>
+      <Card.Text>
+        { children }
+      </Card.Text>
+    </Card.Body>
+  </Card>
+)
+
+const CardPageOther = ({ title, link, children }) => (
+  <Col xl="4" md="6" className="mb-3">
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          <h3>
+            <Link to={ link }>{ title }</Link>
+          </h3>
+        </Card.Title>
+        <blockquote style={{ fontSize: "88%" }}>
+          <Card.Text>
+            { children }
+          </Card.Text>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  </Col>
 )
 
 export default Index
