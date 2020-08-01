77cdffe54183794b01671074e87be3ef30248d84
LeptoFlare <leptoflare@gmail.com>
2020-08-01 19:30:09 -0400

    Create PageContainer for easy container defaults

---
+++ src/components/PageContainer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const PageContainer = ({ children }) => (
+  <div class="section pt-5">
+    <div class="container">{children}</div>
+  </div>
+)
+
+export default PageContainer
