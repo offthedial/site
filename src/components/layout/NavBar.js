6b4e2b62158e3723b3e8271481ed9d7dc7587cea
LeptoFlare <leptoflare@gmail.com>
2020-08-02 14:25:54 -0400

    Update navbar styles

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,7 +5,7 @@ import brand from "src/static/images/brand.svg"
 const NavBrand = () => (
   <div class="navbar-brand">
     <div class="is-hidden-desktop">
-      <a class="navbar-item py-0 pl-5" href="/">
+      <a class="navbar-item py-0 px-5" href="/">
         <img src={brand} alt="" />
       </a>
     </div>
@@ -19,18 +19,18 @@ const NavBrand = () => (
 
 const NavBar = () => (
   <nav
-    class="navbar is-primary is-transparent"
+    class="navbar is-transparent is-primary"
     role="navigation"
     aria-label="main navigation"
   >
-    <div class="container">
+    <div class="container nav-a">
       <NavBrand />
       <div class="navbar-menu is-paddingless">
         <div class="navbar-end">
-          <a href="/idtga" class="navbar-item">
+          <a href="/idtga" class="navbar-item has-text-weight-medium">
             It's Dangerous to go Alone
           </a>
-          <a href="/wl" class="navbar-item">
+          <a href="/wl" class="navbar-item has-text-weight-medium">
             Weakest Link
           </a>
           <a class="navbar-item py-0" href="/discord">
