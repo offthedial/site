3349ae14b189186a4155ed7b143b32ab32d5ab3d
LeptoFlare <leptoflare@gmail.com>
2020-08-01 23:16:53 -0400

    Fix hrefs

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,31 +2,6 @@ import React from "react"
 import discord from "src/static/images/discord.svg"
 import brand from "src/static/images/brand.svg"
 
-const NavBar = ({ siteTitle }) => (
-  <nav
-    class="navbar is-primary is-transparent"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <div class="container">
-      <NavBrand />
-      <div class="navbar-menu is-paddingless">
-        <div class="navbar-end">
-          <a href="/idtga" class="navbar-item">
-            It's Dangerous to go Alone
-          </a>
-          <a href="/idtga" class="navbar-item">
-            Weakest Link
-          </a>
-          <a class="navbar-item py-0" href="/">
-            <img src={discord} alt="" />
-          </a>
-        </div>
-      </div>
-    </div>
-  </nav>
-)
-
 const NavBrand = () => (
   <div class="navbar-brand">
     <div class="is-hidden-desktop">
@@ -40,6 +15,31 @@ const NavBrand = () => (
       </a>
     </div>
   </div>
+)
+
+const NavBar = () => (
+  <nav
+    class="navbar is-primary is-transparent"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="container">
+      <NavBrand />
+      <div class="navbar-menu is-paddingless">
+        <div class="navbar-end">
+          <a href="/idtga" class="navbar-item">
+            It's Dangerous to go Alone
+          </a>
+          <a href="/wl" class="navbar-item">
+            Weakest Link
+          </a>
+          <a class="navbar-item py-0" href="/discord">
+            <img src={discord} alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
 )
 
 export default NavBar
