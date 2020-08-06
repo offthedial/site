a2a3c616a7d831166063f4c4955068a9dbf781c4
LeptoFlare <leptoflare@gmail.com>
2020-08-06 09:38:45 -0400

    Create youtube direct I FUCKING SWEAR THERE WAS ONE FUUUUU thanks stormy

---
+++ src/pages/youtube.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Redirect from "src/components/Redirect"
+
+const Discord = () => (
+  <Redirect to="https://www.youtube.com/channel/UCjFqnfg0CKsnQ8ag1MfNtuQ" />
+)
+
+export default Discord
