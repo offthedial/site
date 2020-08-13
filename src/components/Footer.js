e6bcc18fdff0e58022185beba2069aa57793f35a
LeptoFlare <leptoflare@gmail.com>
2020-08-12 23:05:32 -0400

    Add margin properly FUCK LINE BREAKS ALL MY HOMIES HATE LINE BREAKS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import React from "react"
 
 const Footer = ({ children }) => (
-  <footer class="section py-1">
-    <br />
+  <footer class="section my-5 py-1">
     <hr />
     <div class="container">
       <p class="is-size-7 has-text-grey is-italic">{children}</p>
     </div>
-    <br />
   </footer>
 )
 
