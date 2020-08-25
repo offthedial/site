275355d89cc47337c7afc3e83736980fd22b3cdc
LeptoFlare <leptoflare@gmail.com>
2020-08-24 21:11:24 -0400

    prettier

--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -4,7 +4,10 @@ const Highlight = ({ children }) => (
   <div class="container">
     <div class="columns is-centered">
       <div class="column is-9">
-        <div class="py-6 px-6 has-background-primary" style={{borderRadius: 4}}>
+        <div
+          class="py-6 px-6 has-background-primary"
+          style={{ borderRadius: 4 }}
+        >
           <div class="columns is-vcentered">{children}</div>
         </div>
       </div>
