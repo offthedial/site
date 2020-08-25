2f4e5f7d240d6bfd8fa7da31beb439ae7de0cdc7
LeptoFlare <leptoflare@gmail.com>
2020-08-24 21:06:51 -0400

    Resolve graphql warning

--- src/components/Promoted.js
+++ src/components/Promoted.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Promoted = ({ slug }) => {
   const data = useStaticQuery(graphql`
