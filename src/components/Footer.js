6fae5ada8526d60d44b4cba31ee62ef600d322d8
LeptoFlare <leptoflare@gmail.com>
2020-11-21 22:49:13 -0500

    Update media footer to have a nice off-white bg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Footer = ({ children }) => (
-  <footer class="section my-5 py-1">
+  <footer class="py-5">
     <hr />
     <div class="container">
       <p class="is-size-7 has-text-grey is-italic">{children}</p>
