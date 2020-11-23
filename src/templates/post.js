33ea081e0b1a38c109c2f9b4a7ff48f7a4a3001c
LeptoFlare <leptoflare@gmail.com>
2020-11-22 22:16:34 -0500

    Increase size of post text and update spacing of elements Goddamn sections

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,7 +3,6 @@ import { MDXProvider } from "@mdx-js/react"
 import { addMinutes, format } from "date-fns"
 
 import Layout from "src/components/Layout"
-import PageContainer from "src/components/PageContainer"
 
 import Mention from "src/components/Mention"
 import Footer from "src/components/Footer"
@@ -12,31 +11,30 @@ const shortcodes = { Mention, Footer }
 
 const Post = ({ pageContext, children }) => (
   <Layout pageTitle={pageContext.frontmatter.title}>
-    <PageContainer>
     <div class="section has-text-centered">
-            <h1 class="title">{pageContext.frontmatter.title}</h1>
-            <h2 class="subtitle has-text-weight-normal has-text-grey is-size-5">
-              {pageContext.frontmatter.author} ·{" "}
-              {formatPostDate(pageContext.frontmatter.date)}
-              {pageContext.frontmatter.tournament && (
-                <div>
-                  <a
-                    href={`/${pageContext.frontmatter.tournament}`}
-                    class="button mt-2 is-primary is-outlined"
-                  >
-                    Tournament Page
-                  </a>
-                </div>
-              )}
-            </h2>
+      <h1 class="title">{pageContext.frontmatter.title}</h1>
+      <h2 class="subtitle has-text-weight-normal has-text-grey is-size-5">
+        {pageContext.frontmatter.author} ·{" "}
+        {formatPostDate(pageContext.frontmatter.date)}
+        {pageContext.frontmatter.tournament && (
+          <div>
+            <a
+              href={`/${pageContext.frontmatter.tournament}`}
+              class="button mt-2 is-primary is-outlined"
+            >
+              Tournament Page
+            </a>
           </div>
-
+        )}
+      </h2>
+    </div>
+    <div class="section pt-0">
       <div class="columns is-centered">
-        <div class="column is-7">
+        <div class="column is-6 post-text">
           <MDXProvider components={shortcodes}>{children}</MDXProvider>
         </div>
       </div>
-    </PageContainer>
+    </div>
   </Layout>
 )
 
