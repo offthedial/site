b4d802258235d984592fe08349567aafba5b15cc
LeptoFlare <leptoflare@gmail.com>
2020-11-28 18:21:16 -0500

    This config is safe apparently

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -2,6 +2,13 @@
  * Manages firebase configuration
  */
 const config = {
+  apiKey: "AIzaSyCOiE5yw09RbRjgZf7B_t7rvgGfa49j6fU",
+  authDomain: "off-the-dial-26e93.firebaseapp.com",
+  databaseURL: "https://off-the-dial-26e93.firebaseio.com",
+  projectId: "off-the-dial-26e93",
+  storageBucket: "off-the-dial-26e93.appspot.com",
+  messagingSenderId: "822224581984",
+  appId: "1:822224581984:web:951aa8d85c77e6209e5a05"
 };
 
 export default config;
