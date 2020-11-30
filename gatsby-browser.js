0a87a0256851073a82f9eacebd0a3802c648d6f6
LeptoFlare <leptoflare@gmail.com>
2020-11-30 16:46:21 -0500

    Create AuthContext

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
+import React from "react"
+
+import AuthProvider from "src/components/AuthProvider"
 import "src/static/globals.scss"
+
+export const wrapRootElement = ({ element }) => (
+  <AuthProvider>{element}</AuthProvider>
+)
