b87164e7fb86294e78230b16488da423415aea3c
LeptoFlare <leptoflare@gmail.com>
2020-11-30 21:15:10 -0500

    More auth service updates

--- src/services/firebase/auth/index.js
+++ src/services/firebase/auth/index.js
@@ -1,25 +1,25 @@
 /**
- * Create context provider and export auth functions
+ * Create context provider and export auth
  */
 import React, { useEffect, useState } from "react"
 
 import AuthContext from "src/context/AuthContext"
-import { auth } from "../apps"
-import * as functions from "./functions"
+import { auth as app } from "../apps"
+import * as auth from "./auth"
 
 const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null)
 
   useEffect(() => {
-    auth.onAuthStateChanged(setCurrentUser)
+    app.onAuthStateChanged(setCurrentUser)
   }, [])
 
   return (
-    <AuthContext.Provider value={{ auth: functions, currentUser }}>
+    <AuthContext.Provider value={{ auth, currentUser }}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-export { functions }
-export default AuthProvider
+export { AuthProvider }
+export default auth
