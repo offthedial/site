b9e9ce80ed98ae0b1fd8ba7789c8d8d6ccd4eddd
LeptoFlare <leptoflare@gmail.com>
2020-12-03 17:45:52 -0500

    Move padding to a JumboPad component

---
+++ src/components/JumboPad.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const JumboPad = ({ children }) => (
+  <div class="columns is-centered mt-6">
+    <div class="column is-6">{children}</div>
+  </div>
+)
+
+export default JumboPad
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,21 +2,18 @@ import React from "react"
 
 import Layout from "src/components/Layout"
 import PageContainer from "src/components/PageContainer"
+import JumboPad from "src/components/JumboPad"
 
 const NotFound = () => (
   <Layout pageTitle="404">
-    <div>
-      <PageContainer>
-        <div class="columns mt-5 is-centered">
-          <div class="column is-5">
-            <h1 class="title is-1 has-text-weight-bold">404</h1>
-            <h2 class="subtitle is-2 has-text-weight-bold">
-              There's nothing here.
-            </h2>
-          </div>
-        </div>
-      </PageContainer>
-    </div>
+    <PageContainer>
+      <JumboPad>
+        <h1 class="title is-1 has-text-weight-bold">404</h1>
+        <h2 class="subtitle is-2 has-text-weight-bold">
+          There's nothing here.
+        </h2>
+      </JumboPad>
+    </PageContainer>
   </Layout>
 )
 
