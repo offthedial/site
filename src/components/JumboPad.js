bc4b2baad825a4d8221e7b934e80e0eb6b10577c
LeptoFlare <leptoflare@gmail.com>
2020-12-03 21:54:05 -0500

    Update jumbopad

--- src/components/JumboPad.js
+++ src/components/JumboPad.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const JumboPad = ({ children }) => (
   <div class="columns is-centered mt-6">
-    <div class="column is-6">{children}</div>
+    <div class="column is-7">{children}</div>
   </div>
 )
 
