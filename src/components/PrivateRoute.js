a002ff7e7368ddd992da44199738440353181bbf
LeptoFlare <leptoflare@gmail.com>
2020-12-04 04:07:22 -0500

    Update private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,15 +3,14 @@ import React from "react"
 import { navigate } from "gatsby"
 import AuthContext from "src/context/AuthContext"
 
-const PrivateRoute = ({ children, location }) => {
+const PrivateRoute = ({ component: Component, location, ...rest }) => {
   const { currentUser } = React.useContext(AuthContext)
   const loginRoute = "/profile/login"
 
   if (!currentUser && location.pathname !== loginRoute) {
     navigate(loginRoute)
-    return null
   }
-  return <>{children}</>
+  return <Component {...rest} />
 }
 
 export default PrivateRoute
