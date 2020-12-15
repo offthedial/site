abb9dfc4ba441a817fe9cf651542b519dc472358
LeptoFlare <leptoflare@gmail.com>
2020-12-14 19:52:53 -0500

    use `gatsby-plugin-firebase`

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,21 @@ module.exports = {
     twitter: `@Off_The_Dial`,
   },
   plugins: [
+    /* Firebase */
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyCOiE5yw09RbRjgZf7B_t7rvgGfa49j6fU",
+          authDomain: "off-the-dial-26e93.firebaseapp.com",
+          databaseURL: "https://off-the-dial-26e93.firebaseio.com",
+          projectId: "off-the-dial-26e93",
+          storageBucket: "off-the-dial-26e93.appspot.com",
+          messagingSenderId: "822224581984",
+          appId: "1:822224581984:web:951aa8d85c77e6209e5a05",
+        },
+      },
+    },
     /* Filesystem stuff */
     {
       resolve: `gatsby-source-filesystem`,
--- src/services/firebase/apps.js
+++ src/services/firebase/apps.js
@@ -2,14 +2,9 @@
  * Initialize firebase and it's apps.
  * Exports should be used internally throughout the service.
  */
-import firebase from "firebase/app"
+import firebase from "gatsby-plugin-firebase"
 import "firebase/auth"
 import "firebase/firestore"
-
-import config from "./config"
-if (!firebase.apps.length) {
-  firebase.initializeApp(config)
-}
 
 // Initialize auth
 let auth
