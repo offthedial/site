64e0d6eeb5ea1d9f7cfd056e13ebe482343c679a
LeptoFlare <leptoflare@gmail.com>
2020-12-16 15:20:12 -0500

    Update loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,6 @@
 import React from "react"
 import Layout from "src/components/Layout"
 
-const Loading = () => (
-  <Layout>
-    <div class="hero">
-      <h1>Loading...</h1>
-    </div>
-  </Layout>
-)
+const Loading = () => <Layout></Layout>
 
 export default Loading
