5fbc3439843738f77c7babb0287f3697b8bdecfd
LeptoFlare <leptoflare@gmail.com>
2020-12-18 14:12:05 -0500

    Delete gatsby-node.js

--- gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
-  if (page.path.match(/^\/profile/)) {
-    page.matchPath = `/profile/*`
-    createPage(page)
-  }
-}
