7a970f4397e1fb8081dd2babb51f297485742368
LeptoFlare <leptoflare@gmail.com>
2020-12-18 16:54:11 -0500

    Use config description Replace hard-coded string for description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import Twemoji from "react-twemoji"
 import Layout from "src/components/Layout"
 import Promoted from "src/components/Promoted"
@@ -14,157 +14,170 @@ import Highlight from "src/components/Highlight"
  */
 const promoted = "idtga-dec-2020"
 
-const Index = () => (
-  <Layout mediaFooter={false}>
-    <div class="section is-medium has-background-white-ter">
-      <Highlight>
-        <div class="column is-9">
-          <h1 class="title is-1 is-hidden-mobile has-text-white">
-            Who are we?
-          </h1>
-          <h6 class="title is-3 is-hidden-tablet has-text-white">
-            Who are we?
-          </h6>
-          <p class="subtitle is-4 has-text-white">
-            Off the Dial is a unique tournament organisation for Splatoon 2.
-            Dedicated to providing fresh tournament opportunities for free
-            agents.
-          </p>
-        </div>
-        <div class="column is-3 is-hidden-mobile">
-          <img src="https://assets.otd.ink/logo-nobg.png" alt="" />
-        </div>
-      </Highlight>
-    </div>
-    {promoted && (
+const Index = () => {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    `
+  )
+  return (
+    <Layout mediaFooter={false}>
+      <div class="section is-medium has-background-white-ter">
+        <Highlight>
+          <div class="column is-9">
+            <h1 class="title is-1 is-hidden-mobile has-text-white">
+              Who are we?
+            </h1>
+            <h6 class="title is-3 is-hidden-tablet has-text-white">
+              Who are we?
+            </h6>
+            <p class="subtitle is-4 has-text-white">
+              {siteMetadata.description}
+            </p>
+          </div>
+          <div class="column is-3 is-hidden-mobile">
+            <img src="https://assets.otd.ink/logo-nobg.png" alt="" />
+          </div>
+        </Highlight>
+      </div>
+      {promoted && (
+        <Section>
+          <Promoted slug={promoted} />
+        </Section>
+      )}
       <Section>
-        <Promoted slug={promoted} />
+        <div class="column">
+          <h2 class="title is-2">FAQ</h2>
+          <p class="subtitle is-5">
+            We get asked a lot of questions, so a FAQ necessary. We highly
+            recommend you check out this page.
+          </p>
+          <Link to="/faq" class="button is-link">
+            Go to FAQ
+          </Link>
+        </div>
+        <div class="column">{/*  */}</div>
       </Section>
-    )}
-    <Section>
-      <div class="column">
-        <h2 class="title is-2">FAQ</h2>
-        <p class="subtitle is-5">
-          We get asked a lot of questions, so a FAQ necessary. We highly
-          recommend you check out this page.
-        </p>
-        <Link to="/faq" class="button is-link">
-          Go to FAQ
-        </Link>
-      </div>
-      <div class="column">{/*  */}</div>
-    </Section>
-    <Section>
-      <div class="column">{/*  */}</div>
-      <div class="column has-text-right">
-        <h2 class="title is-2">Posts</h2>
-        <p class="subtitle is-5">
-          Stay up to date with the latest blog posts, news, and updates on
-          everything Off the Dial!
-        </p>
-        <Link to="/posts" class="button is-primary">
-          View Posts
-        </Link>
-      </div>
-    </Section>
-    <SectionBackground>
-      <div class="column is-7 has-text-centered">
-        <h2 class="title is-2">Staff</h2>
-        <p class="subtitle is-5">
-          Off the Dial is thankful to have such an awesome staff team, you can
-          learn more about each of them here.
-        </p>
-        <Link to="/staff" class="button is-primary">
-          Our Staff Team
-        </Link>
-      </div>
-    </SectionBackground>
-    <Section>
-      <div class="column">
-        <h2 class="title is-2">Bot</h2>
-        <p class="subtitle is-5">
-          Our server has a custom Off the Dial Bot used for tournament running
-          assistance. If you would like to know more, we have a page dedicated
-          to about the bot!
-        </p>
-        <Link to="/bot" class="button is-primary">
-          About the Bot
-        </Link>
-      </div>
-      <div class="column">{/*  */}</div>
-    </Section>
-    <Section>
-      <div class="column">{/*  */}</div>
-      <div class="column has-text-right">
-        <h2 class="title is-2">Assets</h2>
-        <p class="subtitle is-5">
-          Many of our assets are public and free. Assets include logos and
-          banners related to Off the Dial and its tournaments. You can find them
-          all right here!
-        </p>
-        <a href="https://assets.otd.ink" class="button is-primary">
-          Assets Repository
-        </a>
-      </div>
-    </Section>
-    <SectionBackground>
-      <div class="column is-7 has-text-centered">
-        <h2 class="title is-2" id="media">
-          Media
-        </h2>
-        <p class="subtitle is-5">
-          Off the Dial has many media outlets, Here's a list of all the
-          platforms you can catch us on. We'd appreciate if you supported us
-          there.
-        </p>
-        <div class="columns is-mobile">
-          <div class="column">
-            <h1>
-              <Link to="/twitch">
-                <i class="fab fa-twitch" />
-              </Link>
-            </h1>
-          </div>
-          <div class="column">
-            <h1>
-              <Link to="/twitter">
-                <i class="fab fa-twitter" />
-              </Link>
-            </h1>
-          </div>
-          <div class="column">
-            <h1>
-              <Link to="/youtube">
-                <i class="fab fa-youtube" />
-              </Link>
-            </h1>
-          </div>
-          <div class="column">
-            <h1>
-              <Link to="/patreon">
-                <i class="fab fa-patreon" />
-              </Link>
-            </h1>
-          </div>
-          <div class="column">
-            <h1>
-              <Link to="/github">
-                <i class="fab fa-github" />
-              </Link>
-            </h1>
+      <Section>
+        <div class="column">{/*  */}</div>
+        <div class="column has-text-right">
+          <h2 class="title is-2">Posts</h2>
+          <p class="subtitle is-5">
+            Stay up to date with the latest blog posts, news, and updates on
+            everything Off the Dial!
+          </p>
+          <Link to="/posts" class="button is-primary">
+            View Posts
+          </Link>
+        </div>
+      </Section>
+      <SectionBackground>
+        <div class="column is-7 has-text-centered">
+          <h2 class="title is-2">Staff</h2>
+          <p class="subtitle is-5">
+            Off the Dial is thankful to have such an awesome staff team, you can
+            learn more about each of them here.
+          </p>
+          <Link to="/staff" class="button is-primary">
+            Our Staff Team
+          </Link>
+        </div>
+      </SectionBackground>
+      <Section>
+        <div class="column">
+          <h2 class="title is-2">Bot</h2>
+          <p class="subtitle is-5">
+            Our server has a custom Off the Dial Bot used for tournament running
+            assistance. If you would like to know more, we have a page dedicated
+            to about the bot!
+          </p>
+          <Link to="/bot" class="button is-primary">
+            About the Bot
+          </Link>
+        </div>
+        <div class="column">{/*  */}</div>
+      </Section>
+      <Section>
+        <div class="column">{/*  */}</div>
+        <div class="column has-text-right">
+          <h2 class="title is-2">Assets</h2>
+          <p class="subtitle is-5">
+            Many of our assets are public and free. Assets include logos and
+            banners related to Off the Dial and its tournaments. You can find
+            them all right here!
+          </p>
+          <a href="https://assets.otd.ink" class="button is-primary">
+            Assets Repository
+          </a>
+        </div>
+      </Section>
+      <SectionBackground>
+        <div class="column is-7 has-text-centered">
+          <h2 class="title is-2" id="media">
+            Media
+          </h2>
+          <p class="subtitle is-5">
+            Off the Dial has many media outlets, Here's a list of all the
+            platforms you can catch us on. We'd appreciate if you supported us
+            there.
+          </p>
+          <div class="columns is-mobile">
+            <div class="column">
+              <h1>
+                <Link to="/twitch">
+                  <i class="fab fa-twitch" />
+                </Link>
+              </h1>
+            </div>
+            <div class="column">
+              <h1>
+                <Link to="/twitter">
+                  <i class="fab fa-twitter" />
+                </Link>
+              </h1>
+            </div>
+            <div class="column">
+              <h1>
+                <Link to="/youtube">
+                  <i class="fab fa-youtube" />
+                </Link>
+              </h1>
+            </div>
+            <div class="column">
+              <h1>
+                <Link to="/patreon">
+                  <i class="fab fa-patreon" />
+                </Link>
+              </h1>
+            </div>
+            <div class="column">
+              <h1>
+                <Link to="/github">
+                  <i class="fab fa-github" />
+                </Link>
+              </h1>
+            </div>
           </div>
         </div>
+      </SectionBackground>
+      <div class="section py-0">
+        <Footer>
+          Built with <Love /> & <a href="https://www.gatsbyjs.org/">Gatsby</a>.
+          This website is currently in beta, more features are to come. Stay
+          tuned!
+        </Footer>
       </div>
-    </SectionBackground>
-    <div class="section py-0">
-      <Footer>
-        Built with <Love /> & <a href="https://www.gatsbyjs.org/">Gatsby</a>.
-        This website is currently in beta, more features are to come. Stay
-        tuned!
-      </Footer>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 const Section = ({ children }) => (
   <div class="section">
