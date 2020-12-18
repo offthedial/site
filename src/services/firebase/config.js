672372901ca4f9424a321615f53607ecdf9bcca5
LeptoFlare <leptoflare@gmail.com>
2020-12-18 14:35:36 -0500

    Change api url

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -2,4 +2,4 @@
  * Manages firebase configuration (yes, it's safe to be in version control)
  */
 
-export const cloudFunctionsApi = `https://us-central1-off-the-dial-26e93.cloudfunctions.net`
+export const cloudFunctionsApi = `https://api.otd.ink`
