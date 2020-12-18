9dc664200df87bf66578b6e0e35353c59e5fbc1c
LeptoFlare <leptoflare@gmail.com>
2020-12-18 16:35:00 -0500

    Completely refactor meta tags

--- src/components/Layout/SEO.js
+++ src/components/Layout/SEO.js
@@ -1,99 +1,58 @@
 import React from "react"
 
 import { graphql, useStaticQuery } from "gatsby"
-import { PropTypes } from "prop-types"
 import { Helmet } from "react-helmet"
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
+function SEO({ title }) {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(
     graphql`
       query {
         site {
           siteMetadata {
             title
             description
+            image
+            siteUrl
+            twitter
           }
         }
       }
     `
   )
 
-  const metaDescription = description || site.siteMetadata.description
-  const metaTitle =
-    `${title} - ${site.siteMetadata.title}` || site.siteMetadata.title
+  const metaTitle = title
+    ? `${title} - ${siteMetadata.title}`
+    : siteMetadata.title
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s - ${site.siteMetadata.title}`}
-      defaultTitle={site.siteMetadata.title}
-      link={[
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "64x64",
-          href: `https://assets.otd.ink/favicon.png`,
-        },
-      ]}
-      meta={[
-        {
-          property: `og:title`,
-          content:
-            `${title} - ${site.siteMetadata.title}` || site.siteMetadata.title,
-        },
-        {
-          property: `og:site_name`,
-          content: site.siteMetadata.title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:image`,
-          content: `https://assets.otd.ink/icon.png?v=2`,
-        },
-        {
-          property: `theme-color`,
-          content: `#5d9194`,
-        },
-      ].concat(meta)}
-    >
+    <Helmet htmlAttributes={{ lang: "en" }}>
+      <meta property="og:type" content="website" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta content={metaTitle} name="title" />
+      <meta content={metaTitle} property="og:title" />
+      <meta content={metaTitle} property="twitter:title" />
+      <meta content={siteMetadata.description} name="description" />
+      <meta content={siteMetadata.description} property="og:description" />
+      <meta content={siteMetadata.description} property="twitter:description" />
+      <meta content={siteMetadata.image} property="og:image" />
+      <meta content={siteMetadata.image} property="twitter:image" />
+      <meta content={siteMetadata.siteUrl} property="og:url" />
+      <meta content={siteMetadata.siteUrl} property="twitter:url" />
+      <meta content={siteMetadata.twitter} property="twitter:creator" />
       <script
         defer
         src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"
       />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content={site.siteMetadata.twitter} />
-      <meta name="twitter:title" content={metaTitle} />
-      <meta name="twitter:description" content={metaDescription} />
-      <meta name="twitter:image" content="https://assets.otd.ink/banner.png" />
-      <meta
-        name="twitter:image:alt"
-        content={`${site.siteMetadata.title} Banner`}
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="64x64"
+        href="https://assets.otd.ink/favicon.png"
       />
     </Helmet>
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string,
 }
 
 export default SEO
