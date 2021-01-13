9395b265d6e74a79655b32ab7a5ec9ff5927b2f9
LeptoFlare <leptoflare@gmail.com>
2021-01-13 13:10:34 -0500

    Update useTourney to dynamically get the latest tournament as well as return registration status and ended status

--- src/app/hooks/useTourney.js
+++ src/app/hooks/useTourney.js
@@ -1,16 +1,46 @@
 import { useQuery } from "react-query"
 import { db } from "../firebase"
+import { fromUnixTime, isPast } from "date-fns"
 
 export default () =>
   useQuery(
     ["tourney"],
     async () => {
-      const doc = await db
+      const docs = await db
         .collection("tournaments")
-        .doc("2KnLtpnPNjz2AE0OxwX5")
+        .orderBy("date", "desc")
+        .limit(1)
         .get()
+      let doc
+      docs.forEach(d => {
+        doc = d
+      })
+      const tourneyData = doc.data()
+      console.log(doc.id, tourneyData)
 
-      return doc.data()
+      const smashggRes = await fetch("https://api.smash.gg/gql/alpha", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer 6e2e86a15c206caf9550f9046a7ac1f9`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `query {
+            tournament(slug:"${tourneyData.smashgg}") {
+              isRegistrationOpen
+              endAt
+            }
+          }`,
+        }),
+      })
+      const smashggData = (await smashggRes.json()).data?.tournament
+      const endAt = fromUnixTime(smashggData?.endAt)
+
+      return {
+        ...tourneyData,
+        isEnded: isPast(endAt),
+        isRegistrationOpen: smashggData?.isRegistrationOpen,
+      }
     },
     {
       staleTime: 5 * 60 * 1000,
