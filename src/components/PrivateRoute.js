edc45726aa859bfefedb6c05c9dd2098746054b9
LeptoFlare <leptoflare@gmail.com>
2021-01-13 21:27:04 -0500

    Optimize PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,25 +1,22 @@
-import React, { useLayoutEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 import { navigate } from "gatsby"
 import { auth } from "src/app/firebase"
 
 const PrivateRoute = ({ location, children }) => {
   const loginRoute = "/profile/login"
+  const [user, setUser] = useState("pending")
+  const unsub = auth.onAuthStateChanged(setUser)
 
-  useLayoutEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (
-        !user &&
-        location.pathname !== loginRoute &&
-        typeof window !== "undefined"
-      ) {
-        navigate(loginRoute, { state: { from: location.pathname } })
-        return <></>
-      }
-    })
-    unsubscribe()
-  }, [])
-
+  if (user === "pending") {
+    return null
+  } else {
+    unsub()
+    if (!user && typeof window !== "undefined") {
+      navigate(loginRoute, { state: { from: location.pathname } })
+      return null
+    }
+  }
   return <>{children}</>
 }
 
