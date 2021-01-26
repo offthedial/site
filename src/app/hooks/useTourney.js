9bc8f902ff5a73014f796ad7852a212e9d8a6c93
LeptoFlare <leptoflare@gmail.com>
2021-01-26 11:15:09 -0500

    Fix critical issues with useTourney resolve security issue fix infinite loop

--- src/app/hooks/useTourney.js
+++ src/app/hooks/useTourney.js
@@ -7,39 +7,20 @@ export default () =>
   useQuery(
     ["tourney"],
     async () => {
-      const tourney = (
-        await db
-          .collection("tournaments")
-          .orderBy("date", "desc")
-          .limit(1)
-          .get()
-      ).docs[0]
-      tourney.ref.onSnapshot(doc => {
+      const tourneyCol = db.collection("tournaments")
+      tourneyCol.onSnapshot(() => {
         queryClient.invalidateQueries(["tourney"])
       })
+      const tourney = (await tourneyCol.orderBy("date", "desc").limit(1).get())
+        .docs[0]
       const tourneyData = tourney.data()
-
-      const smashggRes = await fetch("https://api.smash.gg/gql/alpha", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer 6e2e86a15c206caf9550f9046a7ac1f9`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: `query {
-            tournament(slug:"${tourneyData.smashgg}") {
-              isRegistrationOpen
-              endAt
-            }
-          }`,
-        }),
-      })
-      const smashggData = (await smashggRes.json()).data?.tournament
 
       return {
         ...tourneyData,
-        hasEnded: isPast(fromUnixTime(smashggData?.endAt)),
-        isRegistrationOpen: smashggData?.isRegistrationOpen,
+        hasEnded: isPast(fromUnixTime(tourneyData.smashgg.endAt)),
+        hasClosed: isPast(
+          fromUnixTime(tourneyData.smashgg.registrationClosesAt)
+        ),
         ref: tourney.ref,
       }
     },
