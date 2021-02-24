5024c8cdb83511580a10c22f0c0730774b5f7aad
LeptoFlare <leptoflare@lepto.tech>
2021-02-24 00:33:08 -0500

    Migrate posts page to Chakra

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,39 +1,38 @@
 import React from "react"
 
+import * as Chakra from "@chakra-ui/react"
 import { graphql, Link } from "gatsby"
 import Layout from "src/components/Layout"
-import PageHero from "src/components/PageHero"
+import Title from "src/components/Title"
 
 const Posts = ({ data }) => (
   <Layout pageTitle="Posts">
-    <PageHero title="Posts">
+    <Title title="Posts">
       Check out the latest blog posts, news, and updates from Off the Dial!
       Posts may consist of patch notes, new features or anything else we want to
       say.
-    </PageHero>
-    <div class="section pt-0">
-      <div class="container">
-        <div class="columns is-centered">
-          <div class="column is-10">
-            {data.allMdx.edges.map(({ node }) => (
-              <div class="bg-hover-shade card mx-3 my-3" key={node.parent.name}>
-                <Link to={node.parent.name}>
-                  <div class="card-content">
-                    <h2 class="mb-0">{node.frontmatter.title}</h2>
-                    <p class="has-text-grey mb-3 is-size-5">
-                      {node.frontmatter.author} · {node.frontmatter.date}
-                    </p>
-                    <blockquote class="has-text-grey">
-                      <i>{node.excerpt}</i>
-                    </blockquote>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
+    </Title>
+    <Chakra.Container maxW="container.md">
+      <Chakra.Stack spacing={6}>
+        {data.allMdx.edges.map(({ node }) => (
+          <Link to={node.parent.name} key={node.parent.name}>
+            <Chakra.Box layerStyle="card" p={6} _hover={{ bg: "gray.50" }}>
+              <Chakra.Box fontSize="lg">
+                <Chakra.Text fontSize="2xl" fontWeight="bold" lineHeight="none">
+                  {node.frontmatter.title}
+                </Chakra.Text>
+                <Chakra.Text color="gray.500" mb={2}>
+                  {node.frontmatter.author} · {node.frontmatter.date}
+                </Chakra.Text>
+              </Chakra.Box>
+              <Chakra.Text color="gray.600" as="blockquote" fontStyle="italic">
+                {node.excerpt}
+              </Chakra.Text>
+            </Chakra.Box>
+          </Link>
+        ))}
+      </Chakra.Stack>
+    </Chakra.Container>
   </Layout>
 )
 
