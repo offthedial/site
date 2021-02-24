272957ba54c59205d4c97b578a57d84852451fb8
LeptoFlare <leptoflare@lepto.tech>
2021-02-24 00:31:12 -0500

    Fix media link naming

--- src/pages/twitch.js
+++ src/pages/twitch.js
@@ -2,6 +2,6 @@ import React from "react"
 
 import Redirect from "src/components/Redirect"
 
-const Discord = () => <Redirect to="https://twitch.tv/offthedial" />
+const Twitch = () => <Redirect to="https://twitch.tv/offthedial" />
 
-export default Discord
+export default Twitch
