82c7c7ae91b8ee8df80c63ee984758a635e82b66
LeptoFlare <leptoflare@lepto.tech>
2021-02-24 00:31:45 -0500

    Delete old components

--- src/components/PageContainer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const PageContainer = ({ children }) => (
-  <div class="section pt-5">
-    <div class="container">{children}</div>
-  </div>
-)
-
-export default PageContainer
