0000a3c312abd6b80402c848cc198901575b6730
LeptoFlare <leptoflare@lepto.tech>
2021-02-24 00:32:19 -0500

    Migrate templates to Chakra

--- src/templates/md.js
+++ src/templates/md.js
@@ -1,25 +1,17 @@
 import React from "react"
 
 import { MDXProvider } from "@mdx-js/react"
+import * as Chakra from "@chakra-ui/react"
 import Layout from "src/components/Layout"
-import PageHero from "src/components/PageHero"
-import Mention from "src/components/Mention"
-import Footer from "src/components/Footer"
-
-const shortcodes = { Mention, Footer }
+import Title from "src/components/Title"
 
 const Post = ({ pageContext, children }) => (
   <Layout pageTitle={pageContext.frontmatter.title}>
-    {pageContext.frontmatter.hero && (
-      <PageHero title={pageContext.frontmatter.title}>
-        {pageContext.frontmatter.hero}
-      </PageHero>
-    )}
-    <div class={"section " + (pageContext.frontmatter.hero ? "pt-0" : "pt-5")}>
-      <div class="container">
-        <MDXProvider components={shortcodes}>{children}</MDXProvider>
-      </div>
-    </div>
+    <Chakra.Container maxW="container.lg">
+      <MDXProvider components={{ Title }}>
+        <article>{children}</article>
+      </MDXProvider>
+    </Chakra.Container>
   </Layout>
 )
 
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,42 +1,25 @@
 import React from "react"
 
-import { Link } from "gatsby"
+import * as Chakra from "@chakra-ui/react"
 import { MDXProvider } from "@mdx-js/react"
 import { addMinutes, format } from "date-fns"
 import Layout from "src/components/Layout"
-import PostContent from "src/components/PostContent"
-import Mention from "src/components/Mention"
-import Footer from "src/components/Footer"
-
-const shortcodes = { Mention, Footer }
+import Title from "src/components/Title"
 
 const Post = ({ pageContext, children }) => (
   <Layout pageTitle={pageContext.frontmatter.title}>
-    <div class="section has-text-centered">
-      <h1 class="title">{pageContext.frontmatter.title}</h1>
-      <h2 class="subtitle has-text-weight-normal has-text-grey is-size-5">
-        {pageContext.frontmatter.author} ·{" "}
+    <Title title={pageContext.frontmatter.title}>
+      <Chakra.Text mb={2}>
+        {pageContext.frontmatter.author}
+        {` · `}
         {formatPostDate(pageContext.frontmatter.date)}
-        {pageContext.frontmatter.tournament && (
-          <div class="buttons is-centered">
-            <Link to={`/signup`} class="button mt-2 is-orange is-outlined">
-              Signup!
-            </Link>
-            <Link
-              to={`/${pageContext.frontmatter.tournament}`}
-              class="button mt-2 is-primary is-inverted"
-            >
-              Tournament Page
-            </Link>
-          </div>
-        )}
-      </h2>
-    </div>
-    <div class="section pt-0">
-      <PostContent>
-        <MDXProvider components={shortcodes}>{children}</MDXProvider>
-      </PostContent>
-    </div>
+      </Chakra.Text>
+    </Title>
+    <Chakra.Container fontSize="sl">
+      <MDXProvider components={{ Title }}>
+        <article>{children}</article>
+      </MDXProvider>
+    </Chakra.Container>
   </Layout>
 )
 
