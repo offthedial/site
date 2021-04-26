e20bb8bb59f717ecbed73f7e25dc786ea47652d1
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-04-26 13:27:37 -0400

    Empty pages

--- src/pages/idtga/index.js
+++ src/pages/idtga/index.js
@@ -1,94 +1,324 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+import { useTourney, useUserSignup } from "src/app/hooks"
 
 import { Link } from "gatsby"
+import * as Chakra from "@chakra-ui/react"
 import Layout from "src/components/Layout"
-import PageContainer from "src/components/PageContainer"
-import ScheduleTime from "src/components/ScheduleTime"
-import Highlight from "src/components/Highlight"
+import skate from "src/static/skate.svg"
+import idtga from "src/static/idtga.svg"
 
-const Idtga = () => (
-  <Layout
-    pageTitle="It's Dangerous to go Alone"
-    meta={{
-      image: "https://assets.otd.ink/idtga/logo.svg",
-      description: `It's Dangerous to go Alone is our flagship, solo registration tournament. Focused on creating balanced teams, and being accessible to everyone.`,
-    }}
-  >
-    <PageContainer>
-      <Highlight>
-        <div class="column is-8 has-text-white">
-          <h1 class="has-text-white">About It's Dangerous to go Alone</h1>
-          <p class="is-size-6">
-            <u>It's Dangerous to go Alone</u> is our flagship, solo registration
-            tournament. Focused on creating balanced teams, and being accessible
-            to everyone.
-          </p>
-          <p>All you need to do is sign up! The world is waiting…</p>
-          <div class="buttons">
-            <Link to="/signup" class="button is-medium is-orange is-inverted">
-              Signup
-            </Link>
-            <Link to="/idtga/rules" class="button is-medium is-primary is-text">
-              Rules
-            </Link>
-          </div>
-        </div>
-        <div class="column is-4">
-          <img src="https://assets.otd.ink/idtga/logo-nobg.svg" alt="" />
-        </div>
-      </Highlight>
-      <div class="section px-0">
-        <h2>Registration Details</h2>
-        <p>
-          Since this is a solo registration event, you sign up alone. The
-          tournament organisers will handle assembling teams for you.
-        </p>
-        <h3>Registering as a sub</h3>
-        <p>
-          If you missed the registration deadline, you can still sign up as a
-          sub by going to the same signup link. I highly recommend doing this if
-          you miss the signup date, as many players end up either dropping out
-          or needing a sub.
-        </p>
-        <p>
-          If you are needed, you will recieve a dm asking whether you are
-          available to sub and specific details about what team you will be
-          subbing for. Once you agree, you will be given the competing and team
-          roles, then you should get in contact with your other team members.
-        </p>
-        <p>
-          Please make sure you have turned on DMs for the server, and try your
-          best to avaliable to reply during the tournament.
-        </p>
-      </div>
-      <div class="section px-0">
-        <h2>Schedule</h2>
-        <table class="table">
-          <tbody>
-            <ScheduleTime date="4 Days">
-              Check-in opens and registration <strong>remains open</strong>.
-            </ScheduleTime>
-            <ScheduleTime date="3 Days">
-              Both check-in and registration will close, invalid attendees will
-              be removed, and we start assembling teams.
-            </ScheduleTime>
-            <ScheduleTime date="2 Days">
-              Players receive their teams, and the maplist is published. You can
-              now practice with your team.
-            </ScheduleTime>
-            <ScheduleTime date="1 Hour">
-              Players will be alerted and preparations will commence. We request
-              that you be online on both Splatoon 2 and Discord.
-            </ScheduleTime>
-            <ScheduleTime date="10 Minutes">
-              Stream begins on <Link to="/twitch">twitch</Link>, check it out if
-              you're not playing!
-            </ScheduleTime>
-          </tbody>
-        </table>
-      </div>
-    </PageContainer>
-  </Layout>
+const Idtga = () => {
+  const tourney = useTourney()
+  const userSignup = useUserSignup()
+
+  const [signupButtonText, setSignupButtonText] = useState("Signup")
+  useEffect(() => {
+    if (userSignup.data?.type === "signup") {
+      setSignupButtonText("Update Signup Form")
+    } else if (userSignup.data?.type === "sub") {
+      setSignupButtonText("Update Sub Form")
+    } else if (tourney.data?.hasClosed() && !tourney.data?.hasEnded()) {
+      setSignupButtonText("Signup as a Sub")
+    } else {
+      setSignupButtonText("Signup")
+    }
+  }, [tourney, userSignup])
+
+  // const countdown = useState([null, null, null])
+  // useEffect(() => {
+  //   tourneyData.smashgg.registrationClosesAt
+  // })
+
+  return (
+    <Layout
+      pageTitle="It's Dangerous to go Alone"
+      meta={{
+        image: idtga,
+        description: `It's Dangerous to go Alone is our flagship, solo registration tournament. Focused on creating balanced teams, and being accessible to everyone.`,
+      }}
+    >
+      <Chakra.Box bg="otd.slate.0">
+        <Chakra.Box p={4}>
+          <Chakra.Text
+            fontSize={["4xl", "5xl", null, "6xl"]}
+            fontWeight="bold"
+            color="white"
+            textAlign="center"
+          >
+            It's Dangerous to go Alone
+          </Chakra.Text>
+        </Chakra.Box>
+        <Chakra.Grid
+          templateColumns="repeat(3, minmax(0, 1fr))"
+          pb={[6, null, null, 10]}
+          px={[4, null, null, 10]}
+          gap={[6, null, null, 10]}
+        >
+          <Chakra.GridItem colSpan={[3, 1]} alignSelf="center" h="full">
+            <Chakra.Stack
+              alignItems="center"
+              h="full"
+              justify={["center", null, null, "space-around"]}
+            >
+              <Chakra.Box maxW="xs">
+                <Chakra.Image maxW="full" src={idtga} />
+              </Chakra.Box>
+            </Chakra.Stack>
+          </Chakra.GridItem>
+          <Chakra.GridItem
+            rowStart={[2, 1]}
+            colStart={[null, 2]}
+            colSpan={[3, 2]}
+          >
+            <Chakra.Box p={4} rounded="xl" borderWidth={2} borderColor="white">
+              <Chakra.Flex
+                maxW="lg"
+                direction="column"
+                align="center"
+                justify="center"
+                textAlign="center"
+                mx="auto"
+              >
+                <CardSection>
+                  <CardHeading>Tournament Date:</CardHeading>
+                  <CardText>February 21</CardText>
+                </CardSection>
+                <CardSection>
+                  <CardHeading>Signups Close:</CardHeading>
+                  <Chakra.Stack
+                    direction={["column", "row"]}
+                    justify={["space-around", "space-evenly"]}
+                    spacing={[6, 2]}
+                  >
+                    <CardCount value={5} text="Days" />
+                    <CardCount value={12} text="Hours" />
+                    <CardCount value={32} text="Minutes" />
+                  </Chakra.Stack>
+                </CardSection>
+                <CardSection>
+                  <Chakra.Button
+                    as={Link}
+                    to="/signup"
+                    size="xl"
+                    variant="outline"
+                    rounded={["xl", "2xl"]}
+                    h={[12, 16, null, 20]}
+                    minW={[12, 16, null, 20]}
+                    px={[6, 8, null, 10]}
+                    fontSize={["2xl", null, null, "4xl"]}
+                    fontWeight="normal"
+                    borderWidth={[3, null, null, 4]}
+                    borderColor="otd.pink.100"
+                    bg="otd.pink.0"
+                    color="otd.pink.800"
+                    _hover={{ bg: "otd.pink.200" }}
+                    _active={{ bg: "otd.pink.200" }}
+                    _focus="none"
+                  >
+                    {signupButtonText}
+                  </Chakra.Button>
+                </CardSection>
+                <Chakra.Text
+                  fontSize={["lg", null, null, "xl"]}
+                  color="otd.slate.200"
+                >
+                  For more information, see{" "}
+                  <Chakra.Link
+                    as={Link}
+                    to="/idtga#details"
+                    color="otd.slate.100"
+                  >
+                    details
+                  </Chakra.Link>
+                  .
+                </Chakra.Text>
+              </Chakra.Flex>
+            </Chakra.Box>
+          </Chakra.GridItem>
+        </Chakra.Grid>
+      </Chakra.Box>
+      <Chakra.Box py={[8, null, null, 16]} pl={[8, null, null, 16]}>
+        <Chakra.Grid templateColumns="repeat(12, minmax(0, 1fr))">
+          <Chakra.GridItem rowStart={1} colStart={1} colEnd={13}>
+            <Chakra.Image src={skate} />
+          </Chakra.GridItem>
+          <Chakra.GridItem
+            pt={[4, 0]}
+            fontSize={["3xl", "4xl"]}
+            rowStart={[2, null, null, 1]}
+            colStart={[1, null, null, 5]}
+            colEnd={[13, null, null, 12]}
+            zIndex={1}
+          >
+            <Chakra.Text fontWeight="black">
+              Our flagship,{" "}
+              <Chakra.Text as="span" fontStyle="italic">
+                solo registration
+              </Chakra.Text>{" "}
+              tournament.
+            </Chakra.Text>
+            <Chakra.Text fontSize={["2xl", "3xl"]} textStyle="semimute">
+              Focused on creating balanced teams, and being accessible to
+              everyone.
+            </Chakra.Text>
+          </Chakra.GridItem>
+        </Chakra.Grid>
+      </Chakra.Box>
+      <Whoosh>
+        <WhooshPromo
+          title="No team, no problem"
+          description="From recent team disbands to being brand new to the scene. It's always accessible to gain competitive experience here."
+        >
+          <Chakra.Box p={12} bg="otd.slate.300" />
+        </WhooshPromo>
+        <WhooshPromo
+          title="Put yourself out there"
+          description="Show the scene what you're made of. Test your chemistry with different people, you might just find your new teammates."
+          reversed={true}
+        >
+          <Chakra.Box p={12} bg="otd.slate.300" />
+        </WhooshPromo>
+        <WhooshPromo
+          title="Have some fun"
+          description="Open weekend? Meet some new people, make some new friends, and have a whole lot of fun in a unique kind of tournament!"
+        >
+          <Chakra.Box p={12} bg="otd.slate.300" />
+        </WhooshPromo>
+      </Whoosh>
+      <Chakra.Box p={36}>
+        {/* Updates / Changes from last season */}
+        <Chakra.Alert colorScheme="yellow">
+          <Chakra.AlertTitle>Changes from last season:</Chakra.AlertTitle>
+          <Chakra.AlertDescription>
+            Each swiss round has been shortened from 40 minutes to 30.
+          </Chakra.AlertDescription>
+        </Chakra.Alert>
+        <Chakra.Box>{/* Schedule */}</Chakra.Box>
+      </Chakra.Box>
+    </Layout>
+  )
+}
+
+const CardSection = ({ children }) => (
+  <Chakra.Box pb={8} w="full">
+    {children}
+  </Chakra.Box>
 )
+
+const CardHeading = ({ children }) => (
+  <Chakra.Text
+    color="otd.slate.200"
+    fontSize={["xl", "2xl", null, "3xl"]}
+    fontFamily="mono"
+    letterSpacing={4}
+    lineHeight="sm"
+    textTransform="uppercase"
+  >
+    {children}
+  </Chakra.Text>
+)
+
+const CardText = ({ children, ...rest }) => (
+  <Chakra.Text
+    color="white"
+    fontSize={["4xl", null, null, "5xl"]}
+    lineHeight="none"
+    fontWeight="bold"
+    {...rest}
+  >
+    {children}
+  </Chakra.Text>
+)
+
+const CardCount = ({ text, value }) => (
+  <Chakra.Box flexGrow="1" flexShrink="1" flexBasis="0">
+    <Chakra.Stack>
+      <CardText color="otd.slate.100" fontFamily="mono">
+        {value}
+      </CardText>
+      <CardText fontSize={["2xl", "3xl", null, "4xl"]}>{text}</CardText>
+    </Chakra.Stack>
+  </Chakra.Box>
+)
+
+const Whoosh = ({ children }) => (
+  <>
+    <WhooshWave />
+    <Chakra.Box layerStyle="tint">
+      <Chakra.Container maxW="container.xl">{children}</Chakra.Container>
+    </Chakra.Box>
+    <WhooshWave reversed={true} />
+  </>
+)
+
+const WhooshWave = ({ reversed = false }) => (
+  <Chakra.Box textStyle="tinted">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 500 42"
+      xmlSpace="preserve"
+      fill="currentColor"
+    >
+      <g
+        transform={
+          reversed &&
+          "matrix(-1 -2.2967631E-06 2.2967631E-06 -1 500.5 42.501152)"
+        }
+      >
+        <path
+          d="M499.99 27.4921C484.7 30.6021 480.16 31.7321 464.87 34.8521C463.32 35.1721 461.67 35.4721 460.2 34.9021C455.64 33.1421 457.58 26.0321 461.21 22.7521C464.84 19.4721 469.85 16.1721 469.38 11.3121C468.78 5.13207 460.33 3.79207 454.19 4.74207C420.53 9.96207 391.43 31.8921 357.97 38.2921C354.28 39.0021 349.24 38.7121 348.3 35.0721C347.92 33.6121 348.42 32.0221 347.99 30.5721C346.93 26.9521 341.79 27.1121 338.13 28.0521C328.88 30.4221 319.64 32.8021 310.39 35.1721C304.87 36.5921 297.37 37.1121 295.12 31.8821C291.57 23.6221 307.05 14.2421 301.2 7.42207C299.12 5.00207 295.45 4.93207 292.28 5.23207C253.08 8.87206 218.42 37.9921 179.1 36.0321C174.62 35.8121 168.79 33.5221 169.46 29.0821C169.84 26.5621 172.32 24.8621 173.1 22.4421C175.1 16.1821 165.67 12.9221 159.15 13.7121C130.68 17.1521 104.94 33.2621 76.49 36.7921C72.55 37.2821 67.83 37.1921 65.55 33.9421C60.58 26.8521 73.69 19.7121 74.33 11.0821C74.73 5.71207 69.72 1.11207 64.42 0.22207C59.11 -0.667931 53.75 1.25207 48.9 3.58207C29.08 13.0721 21.89 24.8121 0 26.6921L0 45.3121L499.99 45.3121L499.99 27.4921L499.99 27.4921Z"
+          transform="translate(-6.1035156E-05 3.6879349)"
+        />
+        <path
+          d="M0.95504 6.23343C0.255043 7.35343 -0.29496 8.73343 0.175041 9.97343C0.535042 10.9234 1.43504 11.5834 2.40504 11.8634C3.37505 12.1534 4.40504 12.1134 5.41505 12.0234C7.59505 11.8134 9.82504 11.2934 11.605 10.0134C15.195 7.43342 15.455 1.77343 10.735 0.183425C7.25504 -0.976574 2.57504 3.64343 0.95504 6.23343L0.95504 6.23343Z"
+          transform="translate(81.16496 1.0065727)"
+        />
+        <path
+          d="M0.634155 4.06522C0.314148 4.57521 0.0441589 5.13522 0.00415039 5.73521C-0.0358582 6.33521 0.214142 6.97522 0.734161 7.28521C1.08417 7.49521 1.50415 7.52522 1.91415 7.52522C4.80417 7.53521 7.55417 6.27522 9.96414 4.69521C10.6342 4.25521 11.3441 3.69521 11.4541 2.90521C11.8041 0.405212 7.86414 -0.294788 6.15414 0.105213C3.89417 0.635216 1.85416 2.10522 0.634155 4.06522L0.634155 4.06522Z"
+          transform="translate(317.92584 17.934784)"
+        />
+        <path
+          d="M0.00765991 2.9814C0.0676575 3.5114 0.667664 3.7714 1.18765 3.8914C2.71765 4.2314 4.93765 4.3214 6.34766 3.4814C7.44766 2.8214 7.82767 1.2914 6.71765 0.401402C5.22766 -0.778599 -0.232361 0.811401 0.00765991 2.9814L0.00765991 2.9814Z"
+          transform="translate(338.25235 16.118599)"
+        />
+      </g>
+    </svg>
+  </Chakra.Box>
+)
+
+const WhooshPromo = ({ title, description, reversed = false, children }) => {
+  const props = reversed
+    ? [
+        { colStart: [1, null, 2], colEnd: 3, rowStart: [2, null, 1] },
+        { colStart: 1, colEnd: [3, null, 2] },
+      ]
+    : [
+        { colStart: 1, colEnd: [3, null, 2], rowStart: [2, null, 1] },
+        { colStart: [1, null, 2], colEnd: 3 },
+      ]
+  return (
+    <Chakra.Box px={[4, null, 8, 16]} py={[16, null, 32, 44]}>
+      <Chakra.Grid
+        gap={[4, null, 12, 36]}
+        templateColumns="repeat(2, minmax(0, 1fr))"
+      >
+        <Chakra.GridItem {...props[0]}>
+          <Chakra.Box>
+            <Chakra.Text fontSize={["3xl", "4xl"]} fontWeight="bold" mb={2}>
+              {title}
+            </Chakra.Text>
+            <Chakra.Text fontSize="2xl" textStyle="semimute">
+              {description}
+            </Chakra.Text>
+          </Chakra.Box>
+        </Chakra.GridItem>
+        <Chakra.GridItem {...props[1]}>{children}</Chakra.GridItem>
+      </Chakra.Grid>
+    </Chakra.Box>
+  )
+}
 
 export default Idtga
