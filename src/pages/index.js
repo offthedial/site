4ee8a5600c97abbdac4575d3377b00de9674439a
DJam98 <57510174+DJam98@users.noreply.github.com>
2021-09-27 09:24:55 +1000

    remove error

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,17 +34,17 @@ const Index = () => {
         <Highlight>
           <div class="column is-9">
             <h1 class="title is-1 is-hidden-mobile has-text-white">
-              Known Issue
+              Who are we?
             </h1>
             <h6 class="title is-3 is-hidden-tablet has-text-white">
-              Known Issue
+              Who are we?
             </h6>
             <p class="subtitle is-4 has-text-white">
-              There is a known issue where users can't login with Discord, preventing signups. We are working on this issue. For all updates, please keep an eye on our announcements channel in our Discord server. We apologise for the inconvenience.
+              {siteMetadata.description}
             </p>
           </div>
           <div class="column is-3 is-hidden-mobile">
-            <img src="https://assets.otd.ink/warning.svg" alt="" />
+            <img src="https://assets.otd.ink/logo-nobg.svg" alt="" />
           </div>
         </Highlight>
       </div>
