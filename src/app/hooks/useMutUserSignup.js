508d5542540407d383f4e67849d468a0c8ad8d80
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-12-20 19:02:04 +0000

    update user signup mutation hook

--- src/app/hooks/useMutUserSignup.js
+++ src/app/hooks/useMutUserSignup.js
@@ -19,6 +19,7 @@ export default () => {
         // Set existing signup
         return await userSignup.ref.set({
           signupDate: userSignup.data.signupDate,
+          timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
           modifiedDate: signupDate,
         })
       } else {
@@ -26,7 +27,10 @@ export default () => {
         return await tourney.ref
           .collection(tourney.hasClosed() ? "subs" : "signups")
           .doc(auth.currentUser.uid)
-          .set({ signupDate })
+          .set({
+            signupDate,
+            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          })
       }
     },
     {
