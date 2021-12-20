9ed52ce60beddb06f82f913484333295663a975f
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2021-12-20 02:07:20 +0000

    Finish signup schema

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import * as Chakra from "@chakra-ui/react"
+import { format } from "date-fns"
 import Layout from "src/components/Layout"
 import PrivateRoute from "src/components/PrivateRoute"
 import {
@@ -10,6 +11,15 @@ import {
   FormProvider,
 } from "react-hook-form"
 import Cleave from "cleave.js/react"
+import {
+  useUserData,
+  useMutUserData,
+  useUserJoined,
+  useUserSignup,
+  useMutUserSignup,
+  useTourney,
+} from "src/app/hooks"
+import Link from "src/components/Link"
 
 const Signup = ({ location }) => (
   <PrivateRoute location={location}>
@@ -20,6 +30,7 @@ const Signup = ({ location }) => (
         mx="auto"
         maxWidth="container.xl"
       >
+        <TopAlerts />
         <Chakra.Box rounded={[null, "lg"]} layerStyle={[null, "lifted"]}>
           <Form />
         </Chakra.Box>
@@ -28,14 +39,52 @@ const Signup = ({ location }) => (
   </PrivateRoute>
 )
 
+const TopAlerts = () => {
+  const tourneyQuery = useTourney()
+  const userSignupQuery = useUserSignup()
+  const userJoinedQuery = useUserJoined()
+  const alerts = getAlerts(tourneyQuery, userSignupQuery, userJoinedQuery)
+
+  return (
+    <>
+      {alerts.map((alert, i) => (
+        <Chakra.Alert
+          status={alert.status}
+          mb={[0, 8]}
+          rounded={[null, "lg"]}
+          key={i}
+        >
+          <Chakra.AlertIcon />
+          <Chakra.Text fontSize="lg">{alert.message}</Chakra.Text>
+        </Chakra.Alert>
+      ))}
+    </>
+  )
+}
+
 const Form = () => {
   const formMethods = useForm({
     mode: "onTouched",
   })
 
+  // Prepopulate data
+  const userDataQuery = useUserData()
+  React.useEffect(() => {
+    formMethods.reset(userDataQuery.data?.profile)
+  }, [userDataQuery.data])
+
+  // Submitting form
+  const userDataMut = useMutUserData()
+  const userSignupMut = useMutUserSignup()
+  const onSubmit = data => {
+    userDataMut.mutate(data)
+    userSignupMut.mutate(format(new Date(), "yyyy-MM-dd HH:mm:ss Z"))
+  }
+
   return (
     <FormProvider {...formMethods}>
       <Chakra.Box
+        onSubmit={formMethods.handleSubmit(onSubmit)}
         flexBasis="0"
         flexGrow="4"
         display="flex"
@@ -44,9 +93,11 @@ const Form = () => {
         p={8}
         rounded={[null, "lg"]}
         layerStyle="normal"
+        as="form"
       >
         <Input
-          name="IGN"
+          name="ign"
+          title="IGN"
           desc={`What is your switch in-game name?`}
           validationRules={{
             required: "This field is required",
@@ -59,7 +110,8 @@ const Form = () => {
         />
         <hr />
         <Input
-          name="Friend-code"
+          name="sw"
+          title="Friend-code"
           desc={`What is your switch friend-code?`}
           validationRules={{
             required: "This field is required",
@@ -77,7 +129,8 @@ const Form = () => {
         />
         <hr />
         <Input
-          name="Peak Rank"
+          name="rank"
+          title="Peak Rank"
           desc={`What is the highest rank you've achieved in the last 4 months? (If applicable, include X power)`}
           validationRules={{
             required: "This field is required",
@@ -99,22 +152,25 @@ const Form = () => {
           cleaveOptions={{ delimiter: ".", blocks: [5, 1], uppercase: true }}
         />
         <hr />
-        <InputContainer
-          name="Weapon Pool"
+        <Input
+          name="weapons"
+          title="Weapon Pool"
           desc={`List up to 5 weapons that you would be comfortable playing during the tournament`}
-        >
-          <Chakra.Input size="lg" />
-        </InputContainer>
-        <hr />
-        <InputContainer
-          name="Competitive Experience"
-          desc={`You can also paste your free agent post if you do not have any team experience.`}
-        >
-          <Chakra.Textarea size="lg" />
-        </InputContainer>
+          validationRules={{ required: "This field is required" }}
+        />
         <hr />
         <Input
-          name="Smash.gg User Slug"
+          name="cxp"
+          title="Competitive Experience"
+          desc={`You can also paste your free agent post if you do not have any team experience.`}
+          validationRules={{ required: "This field is required" }}
+        >
+          {props => <Chakra.Textarea size="lg" {...props} />}
+        </Input>
+        <hr />
+        <Input
+          name="smashgg"
+          title="Smash.gg User Slug"
           desc={
             <>
               The 8 characters that are listed on your{" "}
@@ -125,13 +181,11 @@ const Form = () => {
             </>
           }
           validationRules={{
+            required: "This field is required",
+            minLength: { value: 22, message: "This field is required" },
             pattern: {
               value: /^smash\.gg\/user\/[0-9A-Fa-f]{8}$/,
               message: "Invalid user slug",
-            },
-            minLength: {
-              value: 15,
-              message: "This field is required",
             },
           }}
           cleaveOptions={{
@@ -141,34 +195,141 @@ const Form = () => {
           }}
         />
         <hr />
-        <InputContainer signup={true}>
-          <Chakra.Box display="flex" justifyContent="flex-end">
-            <Chakra.Button colorScheme="otd.slate" size="lg">
-              Signup
-            </Chakra.Button>
-          </Chakra.Box>
-        </InputContainer>
+        <Chakra.Box>
+          <SubmitArea />
+        </Chakra.Box>
       </Chakra.Box>
     </FormProvider>
   )
 }
 
-const Input = ({ name, desc, validationRules, cleaveOptions }) => {
-  const idName = name.toLowerCase().replace(/ /g, "-")
+const SubmitArea = () => {
+  const tourneyQuery = useTourney()
+  const userSignupQuery = useUserSignup()
+  const userJoinedQuery = useUserJoined()
+  // Get alert
+  const alerts = getAlerts(tourneyQuery, userSignupQuery, userJoinedQuery)
+  let alert = alerts ? alerts.at(-1) : null
+  // Set alert style
+  const colorWeight = Chakra.useColorModeValue("600", "200")
+  const alertColor = {
+    success: "green",
+    error: "red",
+    info: "blue",
+  }[alert?.status]
+
+  return (
+    <Chakra.Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Chakra.Box>
+        {alert && (
+          <Chakra.Alert status={alert.status} bg="none" pl={0} pr={3}>
+            <Chakra.AlertIcon />
+            <Chakra.Text
+              color={`${alertColor}.${colorWeight}`}
+              fontSize="lg"
+              fontWeight="semibold"
+            >
+              {alert.message}
+            </Chakra.Text>
+          </Chakra.Alert>
+        )}
+      </Chakra.Box>
+      <Chakra.Box>
+        <Chakra.Button
+          colorScheme="otd.slate"
+          size="lg"
+          type="submit"
+          disabled={alert?.status === "error"}
+        >
+          {userSignupQuery.data?.type ? "Update Profile" : "Signup"}
+        </Chakra.Button>
+      </Chakra.Box>
+    </Chakra.Box>
+  )
+}
+
+const getAlerts = (tourneyQuery, userSignupQuery, userJoinedQuery) => {
+  const alerts = []
+  if (tourneyQuery.data?.hasEnded()) {
+    alerts.push({
+      status: "error",
+      message: (
+        <>
+          <b>Registration is closed</b>. Be on the look out for tournaments in
+          the future!
+        </>
+      ),
+    })
+  } else if (!tourneyQuery.isLoading) {
+    if (userSignupQuery.data?.type) {
+      alerts.push({
+        status: "success",
+        message: (
+          <>
+            <b>You are currently signed up!</b> To update your profile
+            information, re-submit this form.
+          </>
+        ),
+      })
+    } else if (!userSignupQuery.isLoading && tourneyQuery.data?.hasClosed()) {
+      alerts.push({
+        status: "info",
+        message: (
+          <>
+            <b>Signups are closed</b>. Don't worry, you can still sign up as a
+            sub!
+          </>
+        ),
+      })
+    }
+  }
+  if (userJoinedQuery.data === false) {
+    alerts.push({
+      status: "error",
+      message: (
+        <>
+          You must be in the Off the Dial discord server to participate.{" "}
+          <Link to="/discord" fontWeight="black" textDecoration="underline">
+            Join the Server
+          </Link>
+          !
+        </>
+      ),
+    })
+  }
+  return alerts
+}
+
+const Input = ({
+  name,
+  title,
+  desc,
+  validationRules,
+  cleaveOptions,
+  children,
+}) => {
   const { control } = useFormContext()
   const {
     field,
     fieldState: { invalid, error },
   } = useController({
-    name: idName,
+    name,
     control,
     rules: validationRules,
     defaultValue: "",
   })
 
   return (
-    <InputContainer name={name} desc={desc}>
-      <StyledCleave {...field} isInvalid={invalid} options={cleaveOptions} />
+    <InputContainer name={title} desc={desc}>
+      {children ? (
+        children({ isInvalid: invalid, ...field })
+      ) : (
+        <StyledCleave {...field} isInvalid={invalid} options={cleaveOptions} />
+      )}
       <Chakra.Box pt={2}>
         {invalid && (
           <Chakra.Text textStyle="error" fontSize="sm">
@@ -180,31 +341,27 @@ const Input = ({ name, desc, validationRules, cleaveOptions }) => {
   )
 }
 
-const InputContainer = ({ name, desc, children, signup = false }) => (
+const InputContainer = ({ name, desc, children }) => (
   <Chakra.Box
     display="flex"
     flexDir={["column", null, "row"]}
     justifyContent="space-between"
   >
-    {signup ? (
-      <Chakra.Box />
-    ) : (
-      <Chakra.Box
-        flexBasis="0"
-        flexGrow="4"
-        gridGap="4"
-        display="flex"
-        flexDir="column"
-        paddingBottom={[4, null, 0]}
-      >
-        <Chakra.Text fontSize={["3xl", "4xl"]} lineHeight="none">
-          {name}
-        </Chakra.Text>
-        <Chakra.Text fontSize={["lg", "xl"]} textStyle="semimute">
-          {desc}
-        </Chakra.Text>
-      </Chakra.Box>
-    )}
+    <Chakra.Box
+      flexBasis="0"
+      flexGrow="4"
+      gridGap="4"
+      display="flex"
+      flexDir="column"
+      paddingBottom={[4, null, 0]}
+    >
+      <Chakra.Text fontSize={["3xl", "4xl"]} lineHeight="none">
+        {name}
+      </Chakra.Text>
+      <Chakra.Text fontSize={["lg", "xl"]} textStyle="semimute">
+        {desc}
+      </Chakra.Text>
+    </Chakra.Box>
     <Chakra.Box flexBasis="0" flexGrow="2" />
     <Chakra.Box flexBasis="0" flexGrow="3">
       {children}
