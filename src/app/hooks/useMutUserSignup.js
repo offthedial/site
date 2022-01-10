c8bf8247b3dee97a4d89bfed92fc1f410c33d24c
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2022-01-10 03:22:10 +0000

    Add modified date to signup form

--- src/app/hooks/useMutUserSignup.js
+++ src/app/hooks/useMutUserSignup.js
@@ -30,6 +30,7 @@ export default () => {
           .set({
             signupDate,
             timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            modifiedDate: signupDate,
           })
       }
     },
