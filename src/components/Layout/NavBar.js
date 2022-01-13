eb423de1d6d7a69f3d5ebbf8494b023f6f460e9e
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2022-01-13 02:08:00 +0000

    Fix navbar hopefully

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -62,7 +62,9 @@ const MenuLinks = ({ isOpen, avatar }) => (
         <Chakra.Button
           color="otd.slate.50"
           leftIcon={<DiscordIcon />}
-          rightIcon={<FontAwesomeIcon icon={["fas", "chevron-right"]} />}
+          rightIcon={
+            <FontAwesomeIcon width="10px" icon={["fas", "chevron-right"]} />
+          }
           variant="outline"
           _hover={{ background: "#5865F2" }}
           _active={{ background: "#5865F2" }}
@@ -165,10 +167,9 @@ const UserDefault = () => (
     _hover={{ color: "otd.slate.600" }}
     _active={{ color: "otd.slate.700" }}
     icon={
-      <svg
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
+      <Chakra.Icon
+        width={10}
+        height={10}
         viewBox="0 0 20 20"
         fill="currentColor"
       >
@@ -177,7 +178,7 @@ const UserDefault = () => (
           d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
           clipRule="evenodd"
         />
-      </svg>
+      </Chakra.Icon>
     }
   />
 )
