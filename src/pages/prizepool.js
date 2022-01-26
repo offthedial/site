e3b680cbb14a25a0fb2397489be494b9feeb8607
DJam98 <djam98@otd.ink>
2022-01-26 12:53:55 +1100

    Update link

--- src/pages/prizepool.js
+++ src/pages/prizepool.js
@@ -3,7 +3,7 @@ import React from "react"
 import Redirect from "src/components/Redirect"
 
 const Feedback = () => (
-  <Redirect to="https://paypal.me/djam98" />
+  <Redirect to="https://www.paypal.com/donate/?hosted_button_id=M2HMPHXL3BGM8" />
 )
 
 export default Feedback
