1b825056cdb38c8e0ca49ab9c3733df552e75de5
DJam98 <djam98@otd.ink>
2022-01-31 20:07:47 +1100

    Control Z

--- src/pages/staff/apply.js
+++ src/pages/staff/apply.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
 
-import Redirect from "src/components/Redirect"
+import * as typeformEmbed from "@typeform/embed"
 
-const Feedback = () => (
-  <Redirect to="https://forms.gle/TiregKvhAf1CCHwA8" />
-)
+const StaffApply = () => {
+  const typeformRef = useRef(null)
+
+  useEffect(() => {
+    typeformEmbed.makeWidget(
+      typeformRef.current,
+      "https://form.typeform.com/to/bpH2Ai1E",
+      {
+        buttonText: "Let's start",
+        hideScrollbars: true,
+        hideFooter: true,
+        hideHeaders: true,
+        opacity: 100,
+      }
+    )
+  }, [typeformRef])
+
+  return <div ref={typeformRef} style={{ height: "100vh", width: "100%" }} />
+}
 
 export default StaffApply
