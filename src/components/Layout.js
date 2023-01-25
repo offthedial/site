bf158fc28457b612b1b1bc67502c3441f9ad2e17
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2023-01-25 00:29:28 -0500

    Update layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -142,7 +142,7 @@ export const Avatar = ({ className }) => {
         <img
           src={discord.data.avatarUrl}
           className="absolute inset-0 rounded-full shadow-inner"
-          alt="Avatar"
+          alt=""
         />
       )}
     </Link>
@@ -167,20 +167,21 @@ const Footer = () => (
       <Link className="hover:underline" to="/terms">
         Terms
       </Link>
-      <a className="hover:underline" href="https://assets.otd.ink/">
-        Assets
-      </a>
       <ThemePicker />
     </div>
     <div className="flex w-full max-w-sm items-center justify-evenly text-slate-400 dark:text-slate-600">
-      <Link to="/patreon">
+      <Link to="/shop">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
+          viewBox="0 0 20 20"
           fill="currentColor"
-          className="h-5 hover:text-[#ff424d]"
+          className="h-5 w-5 hover:text-otd-cyan"
         >
-          <path d="M512 194.8c0 101.3-82.4 183.8-183.8 183.8-101.7 0-184.4-82.4-184.4-183.8 0-101.6 82.7-184.3 184.4-184.3C429.6 10.5 512 93.2 512 194.8zM0 501.5h90v-491H0v491z" />
+          <path
+            fillRule="evenodd"
+            d="M6 5v1H4.667a1.75 1.75 0 00-1.743 1.598l-.826 9.5A1.75 1.75 0 003.84 19H16.16a1.75 1.75 0 001.743-1.902l-.826-9.5A1.75 1.75 0 0015.333 6H14V5a4 4 0 00-8 0zm4-2.5A2.5 2.5 0 007.5 5v1h5V5A2.5 2.5 0 0010 2.5zM7.5 10a2.5 2.5 0 005 0V8.75a.75.75 0 011.5 0V10a4 4 0 01-8 0V8.75a.75.75 0 011.5 0V10z"
+            clipRule="evenodd"
+          />
         </svg>
       </Link>
       <Link to="/twitch">
